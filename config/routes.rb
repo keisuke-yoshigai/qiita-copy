eba3fde746a3ada149cf1ec16e15c3f69d21d0e3
keisuke-yoshigai <KeisukeYoshigai@Keisuke.local>
2017-06-28 11:59:04 +0900

    Add route and action

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'posts#index'
   resources :users, only: :show
-  resources :posts, only: :new
+  resources :posts, only: [:new, :show]
   devise_for :users
 end
