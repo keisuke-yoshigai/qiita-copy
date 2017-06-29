cf6504fe42f82a8e55953fd883e739151ddb4268
keisuke-yoshigai <KeisukeYoshigai@Keisuke.local>
2017-06-29 15:00:31 +0900

    edited unicorn.rb

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,19 +1,17 @@
-app_path = File.expand_path('../../../', __FILE__)
+app_path = File.expand_path('../../', __FILE__)
 
 worker_processes 1
 
-pid "#{app_path}/shared/tmp/pids/unicorn.pid"
-stderr_path "#{app_path}/shared/log/unicorn.stderr.log"
-stdout_path "#{app_path}/shared/log/unicorn.stdout.log"
+working_directory app_path
+pid "#{app_path}/tmp/pids/unicorn.pid"
+stderr_path "#{app_path}/log/unicorn.stderr.log"
+stdout_path "#{app_path}/log/unicorn.stdout.log"
 
 listen 3000
 timeout 60
 
 preload_app true
 GC.respond_to?(:copy_on_write_friendly=) && GC.copy_on_write_friendly = true
-
-app_root = "/var/www/qiita-copy"
-working_directory app_root
 
 check_client_connection false
 
