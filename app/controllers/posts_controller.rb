23dcd81e1e6cb7d27f2ec252cc0310711f719b4d
keisuke-yoshigai <KeisukeYoshigai@Keisuke.local>
2017-06-30 13:05:34 +0900

    Added flash message

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,6 +10,24 @@ class PostsController < ApplicationController
   end
 
   def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_strong_params)
+    if @post.save
+      flash[:notice] = "投稿しました"
+      redirect_to action: :index
+    else
+      flash.now[:alert] = "投稿できませんでした"
+      render "posts/new"
+    end
+  end
+
+  private
+
+  def post_strong_params
+    params.require(:post).permit(:title, :body)
   end
 
 end
