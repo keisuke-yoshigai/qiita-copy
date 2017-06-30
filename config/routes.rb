4dc5884ff030d5342d158ed7533110323711016e
keisuke-yoshigai <KeisukeYoshigai@Keisuke.local>
2017-06-30 13:07:33 +0900

    Added route

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   root 'posts#index'
   devise_for :users
   resources :users, only: :show
-  resources :posts, only: [:new, :show]
+  resources :posts, only: [:index, :new, :show, :create]
 
 end
