dae01b0d5c18b2ebc3cfc8e4a3f75b95bfc00d76
keisuke-yoshigai <KeisukeYoshigai@Keisuke.local>
2017-07-10 11:15:30 +0900

    Created keyword table

---
+++ db/migrate/20170708074215_create_keywords.rb
@@ -0,0 +1,9 @@
+class CreateKeywords < ActiveRecord::Migration[5.0]
+  def change
+    create_table :keywords do |t|
+      t.string :word, null: false
+      t.references :user, foreign_key: true
+      t.timestamps
+    end
+  end
+end
