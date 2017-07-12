1f04675c12929050a1db53e015f670417c7b4b01
keisuke-yoshigai <KeisukeYoshigai@Keisuke.local>
2017-07-12 15:21:39 +0900

    Created registration keyword function

---
+++ app/controllers/keywords_controller.rb
@@ -0,0 +1,38 @@
+class KeywordsController < ApplicationController
+
+  def index
+    @keyword = Keyword.new
+  end
+
+  def create
+    @keyword = Keyword.new(keyword_params)
+    if @keyword.save
+      redirect_to keywords_path, notice: 'キーワードを追加しました'
+    else
+      flash[:alert] = 'キーワードを追加できませんでした'
+      render :index
+    end
+  end
+
+  def edit
+    @keyword = Keyword.find(keyword_params)
+    if @keyword.save
+      redirect_to :new, notice: 'キーワードを編集しました'
+    else
+      flash[:alert] = 'キーワードを編集できませんでした'
+      render :new
+    end
+  end
+
+  def destroy
+    @keyword = Keyword.find(params[:id])
+    @keyword.destroy
+    redirect_to keywords_path, notice: 'キーワードを削除しました'
+  end
+
+  private
+
+  def keyword_params
+    params.require(:keyword).permit(:word).merge(user_id: current_user.id)
+  end
+end
