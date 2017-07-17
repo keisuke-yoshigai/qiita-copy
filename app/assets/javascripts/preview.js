6e387fac26711ab4aa225e61b9b1c877945f0f61
keisuke-yoshigai <KeisukeYoshigai@Keisuke.local>
2017-07-17 19:40:15 +0900

    Edit to display preview image

--- app/assets/javascripts/preview.js
+++ app/assets/javascripts/preview.js
@@ -1,10 +1,13 @@
 $(document).on('turbolinks:load', function(){
+  var $cheetSheetMarkdown = $('.main-new__body__right__cheetsheet__markdown');
+  var $cheetSheetMarkdownImage = $('.main-new__body__right__cheetsheet__image');
+  var $cheetSheetTitle = $('.main-new__body__right__cheetsheet__title');
+  var $cheetSheetExample = $('.main-new__body__right__cheetsheet__example');
+
+
   $('.main-new__body__left__text__input').on('keyup', function(e){
     e.preventDefault();
     var input = $(this).val();
-    var $cheetSheetMarkdown = $('.main-new__body__right__cheetsheet__markdown');
-    var $cheetSheetTitle = $('.main-new__body__right__cheetsheet__title');
-    var $cheetSheetExample = $('.main-new__body__right__cheetsheet__example');
 
     $.ajax({
       type: 'GET',
@@ -24,11 +27,25 @@ $(document).on('turbolinks:load', function(){
         $cheetSheetMarkdown.empty();
         $cheetSheetTitle.show();
         $cheetSheetExample.show();
+        $cheetSheetMarkdownImage.empty();
       }
     })
     .fail(function(){
       console.log('fail');
     });
+  });
+
+  $('#file_field').on('change', function(){
+    var selectedFile = this.files[0]; //filesでinputタグで選択されたユーザーのローカルのファイル情報を取得。
+    var fileReader = new FileReader();  //FileReaderオブジェクトを生成し、fileReaderに格納。
+    fileReader.onload = function(event){  // fileReaderに読み込みが成功した後の動作を定義
+      var loadedImageUrl = event.target.result; // eventオブジェクトの実際にイベントが発生したDOM要素をtargetプロパティで取得。ここではFileReaderオブジェクトが取れている。その後、resultに格納されている文字列情報をloadedImageUrlに格納している。
+      var imgTag = "<img class='addImage' src=" + loadedImageUrl + ">"  // loadedImageUrlをsrcとするimgタグをappendするために文字列として生成しimgTagに格納。
+      $cheetSheetTitle.hide();
+      $cheetSheetExample.hide();
+      $cheetSheetMarkdownImage.append(imgTag);
+    }
+    fileReader.readAsDataURL(selectedFile); //selectedFileのファイルを読み込み、そのファイルデータを表す文字列がresultに格納される。
   });
 });
 
