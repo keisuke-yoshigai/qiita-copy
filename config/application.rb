759870507c5add4226deea0ea3f9c8d4b4d87854
keisuke-yoshigai <KeisukeYoshigai@Keisuke.local>
2017-07-21 15:18:42 +0900

    Edit application.rb to set japanese time

--- config/application.rb
+++ config/application.rb
@@ -1,17 +1,14 @@
 require_relative 'boot'
-
 require 'rails/all'
 
-# Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module QiitaCopy
   class Application < Rails::Application
     config.i18n.default_locale = :ja
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+      config.time_zone = 'Asia/Tokyo'
+      config.active_record.default_timezone = :local
+
     config.generators do |g|
       g.javascripts false
       g.helper false
