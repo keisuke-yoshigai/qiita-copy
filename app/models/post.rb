c30dcf593d82e4f783a1291519731482df98fc03
keisuke-yoshigai <KeisukeYoshigai@Keisuke.local>
2017-07-21 15:16:21 +0900

    Created association to comment

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,7 @@ class Post < ApplicationRecord
   validates :body, presence: true
   belongs_to :user
   has_many :likes, dependent: :destroy
+  has_many :comments
   mount_uploader :image, ImageUploader
 
   def like_user(user_id)
