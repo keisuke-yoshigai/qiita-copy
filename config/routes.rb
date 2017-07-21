9316a1d5c073f03bd9382174bc8e901177bfdedd
keisuke-yoshigai <KeisukeYoshigai@Keisuke.local>
2017-07-21 21:05:03 +0900

    Added route for search post

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root 'posts#index'
   get '/posts/show_matching_keyword' , to: 'posts#show_matching_keyword'
+  get 'posts/search_post', to: 'posts#search_post'
   devise_for :users
   resources :users, only: :show
   resources :posts, only: [:index, :new, :show, :create] do
