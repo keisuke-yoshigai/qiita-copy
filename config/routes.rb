1513392588c1d9d36f8768243c270ec9ca395cfc
keisuke-yoshigai <KeisukeYoshigai@Keisuke.local>
2017-07-21 15:15:45 +0900

    Created route to for comment

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   resources :users, only: :show
   resources :posts, only: [:index, :new, :show, :create] do
     resources :likes, only: [:create, :destroy]
+    resources :comments, only: :create
   end
   resources :keywords, only: [:index, :create, :edit, :update, :destroy]
 end
